a0a5500f750c043f979ad52dddf9b63f236ed379
mposada97 <85133902+mposada97@users.noreply.github.com>
2026-01-20 19:05:15 +0000

    moved files into folders

--- day3.py
+++
@@ -1,9 +0,0 @@
-print("Welcome to treasure island")
-if input("left or right?") == "right":
-    print("game over")
-elif input("swim or wait?") == "swim":
-    print("game over")
-elif input("which door?") == "yellow":
-    print("you win")
-else:
-    print("Game over")
