9a6a626b5efbbc617d206ae44e74eeeb43fdb19e
mposada97 <85133902+mposada97@users.noreply.github.com>
2026-01-27 21:14:51 +0000

    started blackjack project

---
+++ day11/day11.py
@@ -0,0 +1,5 @@
+# simplified jack black
+# instead of cards we are using the numbers that correspond to the card
+# we are using an infinite deck based on the following:
+cards = [11,2,3,4,5,6,7,8,9,10,10,10,10] 
+
